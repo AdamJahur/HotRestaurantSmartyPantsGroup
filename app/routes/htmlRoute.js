00cbf2b6b892c1a5058e145cf3cf066f2737454a
Alex Rivera <alexis_rivera_31@hotmail.com>
2017-01-18 19:36:28 -0500

    added two data files instead of one

--- app/routes/htmlRoute.js
+++ app/routes/htmlRoute.js
@@ -9,4 +9,8 @@ module.exports = function (app) {
 	app.get('/tables', function(req, res) {
 		res.sendFile(path.join(_dirname + '../app/public/tables.html'));
 	});
+
+	app.get('/reserve', function(req, res) {
+		res.sendFile(path.join(_dirname + '../app/public/reserve.html'));
+	});
 };
