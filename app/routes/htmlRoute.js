ed4167a9d7730fb5e69606dcdcaec41e652a397c
Alex Rivera <alexis_rivera_31@hotmail.com>
2017-01-18 19:39:22 -0500

    fixed routing url

--- app/routes/htmlRoute.js
+++ app/routes/htmlRoute.js
@@ -3,14 +3,14 @@ var path = require('path');
 module.exports = function (app) {
 
 	app.get('/', function(req, res) {
-		res.sendFile(path.join(_dirname + '../app/public/home.html'));
+		res.sendFile(path.join(_dirname + '/../public/home.html'));
 	});
 
 	app.get('/tables', function(req, res) {
-		res.sendFile(path.join(_dirname + '../app/public/tables.html'));
+		res.sendFile(path.join(_dirname + '/../public/tables.html'));
 	});
 
 	app.get('/reserve', function(req, res) {
-		res.sendFile(path.join(_dirname + '../app/public/reserve.html'));
+		res.sendFile(path.join(_dirname + '/../public/reserve.html'));
 	});
 };
